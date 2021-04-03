2ca4641bfdfedb275e87e251c2b8d0211242e816
Matwhils <matheo.douillez1@gmail.com>
2021-04-03 14:33:03 +0200

    c

--- custom.js
+++ custom.js
@@ -1,6 +1,6 @@
 function result(){
-    let pseudo = document.getElementById('pseudo');
-    let password = document.getElementById('password');
+    let pseudo = document.getElementById('pseudo').Value;
+    let password = document.getElementById('password').Value;
 
     sessionStorage.setItem('Pseudo',pseudo);
     sessionStorage.setItem('Password', password);
